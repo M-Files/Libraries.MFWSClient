c0340afc08b9a642e3db01cc0de985cb35a64622
Victor Vogelpoel <victor.vogelpoel@theone.nl>
2020-08-19 14:10:36 +0200

    Add global suppression for CA1712:Do not prefix enum values with type name

---
+++ MFaaP.MFWSClient/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "M-Files standard", Scope = "module")]
